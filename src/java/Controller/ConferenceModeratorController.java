d82540dcf59c8503f5372ff5d55a33ac7b726aa8
rols <znjrols5@gmail.com>
2017-06-26 23:48:28 +0200

    csvreader works

--- src/java/Controller/ConferenceModeratorController.java
+++ src/java/Controller/ConferenceModeratorController.java
@@ -15,11 +15,14 @@ import Model.Room;
 import Model.SessionConf;
 import Model.User;
 import Model.User.UserType;
+import com.mchange.io.FileUtils;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,6 +30,11 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -113,8 +121,222 @@ public class ConferenceModeratorController {
             jsonParse();
         }
         if (ext.equals(".csv")) {
-            //csvParse();
+            csvParse();
         }
+    }
+    
+    /**
+    * returns a row of values as a list
+    * returns null if you are past the end of the input stream
+    */
+    public static List<String> parseLine(Reader r) throws Exception {
+        int ch = r.read();
+        while (ch == '\r') {
+            //ignore linefeed chars wherever, particularly just before end of file
+            ch = r.read();
+        }
+        if (ch<0) {
+            return null;
+        }
+        Vector<String> store = new Vector<String>();
+        StringBuffer curVal = new StringBuffer();
+        boolean inquotes = false;
+        boolean started = false;
+        while (ch>=0) {
+            if (inquotes) {
+                started=true;
+                if (ch == '\"') {
+                    inquotes = false;
+                }
+                else {
+                    curVal.append((char)ch);
+                }
+            }
+            else {
+                if (ch == '\"') {
+                    inquotes = true;
+                    if (started) {
+                        // if this is the second quote in a value, add a quote
+                        // this is for the double quote in the middle of a value
+                        curVal.append('\"');
+                    }
+                }
+                else if (ch == ',') {
+                    store.add(curVal.toString());
+                    curVal = new StringBuffer();
+                    started = false;
+                }
+                else if (ch == '\r') {
+                    //ignore LF characters
+                }
+                else if (ch == '\n') {
+                    //end of a line, break out
+                    break;
+                }
+                else {
+                    curVal.append((char)ch);
+                }
+            }
+            ch = r.read();
+        }
+        store.add(curVal.toString());
+        return store;
+    }
+    
+    private void csvParse() {
+        
+        
+        InputStream is = new ByteArrayInputStream(conferenceFile.getContents());
+        BufferedReader bs = new BufferedReader(new InputStreamReader(is));
+        
+        List<String> lines = new ArrayList<>();
+        bs.lines().forEach(lines::add);
+        Session session = HibernateHelper.getFactory().openSession();
+            Transaction tx = null;
+            Integer userID = null;
+         try {
+             tx = session.beginTransaction();
+             
+             
+             List<String> firstLine = parseLine(new StringReader(lines.get(0)));
+             
+             if (firstLine.get(0).equals("SessionName")) {
+                 for (int i = 1; i < lines.size(); i ++) {
+                     List<String> currLine = parseLine(new StringReader(lines.get(i)));
+                     SessionConf sessionConf = new SessionConf();
+                     sessionConf.setConference(selectedConference);
+                     
+                     for (int j = 0 ; j < currLine.size(); j ++ ) {
+                        if (firstLine.get(j).equals("SessionName")) {
+                            String sessionName = currLine.get(j);
+                            sessionConf.setName(sessionName);
+                        }
+                        if (firstLine.get(j).equals("Type")) {
+                            String type = currLine.get(j);
+                            sessionConf.setType(type);
+                        }
+                        if (firstLine.get(j).equals("Room")) {
+                            try {
+                                String room = currLine.get(j);
+                                Integer roomId = Integer.parseInt(room);
+                                for(Room roomObj : rooms) {
+                                    if (roomObj.getId() == roomId) {
+                                        sessionConf.setRoom(roomObj);
+                                        break;
+                                    }
+                                }
+                            } catch (Exception e) {}
+                        }
+                        if (firstLine.get(j).equals("Date")) {
+                            try {
+                                String dateStr = currLine.get(j);
+                                DateFormat df = new SimpleDateFormat("DD/MM/yyyy");
+                                Date date = df.parse(dateStr);
+                                sessionConf.setDate(date);
+                            } catch (Exception e) {}
+                        }
+                        if (firstLine.get(j).equals("StartTime")) {
+                            try {
+                                String timeStr = currLine.get(j);
+                                DateFormat df = new SimpleDateFormat("hh:mm:ss aa");
+                                Date time = df.parse(timeStr);
+                                sessionConf.setStartTime(time);
+                            } catch (Exception e) {}
+                        }
+                        if (firstLine.get(j).equals("EndTime")) {
+                            try {
+                                String timeStr = currLine.get(j);
+                                DateFormat df = new SimpleDateFormat("hh:mm:ss aa");
+                                Date time = df.parse(timeStr);
+                                sessionConf.setEndTime(time);
+                            } catch (Exception e) {}
+                        }
+
+                        sessions.add(sessionConf);
+                        session.save(sessionConf);
+
+                        if (sessionConf.getRoom() != null) {
+                            sessionConf.setRoomName(sessionConf.getRoom().getName());
+                        }
+                     }
+                 }
+             }
+             else {
+                 for (int i = 1; i < lines.size(); i ++) {
+                     List<String> currLine = parseLine(new StringReader(lines.get(i)));
+                     Presentation presentation = new Presentation();
+                     String authors = null; //process last
+                     
+                     for (int j = 0 ; j < currLine.size(); j ++ ) {
+                        if (firstLine.get(j).equals("PresentationName")) {
+                            String presentationName = currLine.get(j);
+                            presentation.setName(presentationName);
+                        }
+                        if (firstLine.get(j).equals("SessionName")) {
+                            String sessionName = currLine.get(j);
+                            for (SessionConf sessionConf : sessions) {
+                                if (sessionConf.getName().equals(sessionName)) {
+                                    presentation.setSession(sessionConf);
+                                    sessionConf.getPresentations().add(presentation);
+                                    break;
+                                }
+                            }
+                        }
+                        if (firstLine.get(j).equals("Authors")) {
+                            authors = currLine.get(j);
+                        }
+                        if (firstLine.get(j).equals("StartTime")) {
+                            String timeStr = currLine.get(j);
+                            DateFormat df = new SimpleDateFormat("hh:mm:ss aa");
+                            Date time = df.parse(timeStr);
+                            presentation.setStartTime(time);
+                        }
+                     }
+                     
+                     session.save(presentation);
+                    presentations.add(presentation);
+                    
+                    for (String singleAuthor : authors.split(", ")) {
+                        User user = User.FindByNameSurname(
+                                singleAuthor.split(" ")[0], 
+                                singleAuthor.split(" ").length > 1 ? singleAuthor.split(" ")[1] : "");
+
+                        if (user == null) {
+                            user = new User();
+                            user.setFirst_name(singleAuthor.split(" ")[0]);
+                            user.setLast_name(singleAuthor.split(" ").length > 1 ? singleAuthor.split(" ")[1] : "");
+                            user.setType(User.UserType.USER.ordinal());
+                            session.save(user);
+                        }
+
+                        AuthorPresentation authPres = new AuthorPresentation();
+                        authPres.setAuthor(user);
+                        authPres.setPresentation(presentation);
+
+                        session.save(authPres);
+                        presentation.getAuthors().add(authPres);
+                    }
+                    
+                    
+                    for (AuthorPresentation authPres : presentation.getAuthors()) {
+                        presentation.getAuthorNames().add(authPres.getAuthor());
+                    }
+                    presentation.setSessionName(presentation.getSession().getName());
+                 }
+                    
+             }
+             
+             tx.commit();
+         } catch (HibernateException e) {
+             if (tx!=null) tx.rollback();
+             e.printStackTrace();
+
+          }catch (Exception ex) {
+             Logger.getLogger(ConferenceModeratorController.class.getName()).log(Level.SEVERE, null, ex);
+         }finally {
+             session.close(); 
+          }
+        
     }
     
     private void jsonParse() {
